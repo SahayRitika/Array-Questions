3eea80de4e871ead57f4e25e3f9e22970d0a589c
Ritika Sahay <113190073+SahayRitika@users.noreply.github.com>
2024-10-27 14:41:13 +0530

    Create Leetcode 1277. Count Square Sub Matrices with All Ones.java
    Given a m * n matrix of ones and zeros, return how many square submatrices have all ones.
    
     
    
    Example 1:
    
    Input: matrix =
    [
      [0,1,1,1],
      [1,1,1,1],
      [0,1,1,1]
    ]
    Output: 15
    Explanation: 
    There are 10 squares of side 1.
    There are 4 squares of side 2.
    There is  1 square of side 3.
    Total number of squares = 10 + 4 + 1 = 15.
    Example 2:
    
    Input: matrix = 
    [
      [1,0,1],
      [1,1,0],
      [1,1,0]
    ]
    Output: 7
    Explanation: 
    There are 6 squares of side 1.  
    There is 1 square of side 2. 
    Total number of squares = 6 + 1 = 7.
     
    
    Constraints:
    
    1 <= arr.length <= 300
    1 <= arr[0].length <= 300
    0 <= arr[i][j] <= 1

---
+++ Leetcode 1277. Count Square Sub Matrices with All Ones.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int countSquares(int[][] matrix) {
+        int n= matrix.length;//rows
+        int m= matrix[0].length;//columns
+
+        int[][] dp= new int[n][m];
+        int ans=0;
+
+        for(int i=0; i<n; i++){
+            dp[i][0]= matrix[i][0];
+            ans+= matrix[i][0];
+        }
+
+        for(int j=1; j<m; j++){
+            dp[0][j]= matrix[0][j];
+            ans+= matrix[0][j];
+        }
+
+        //Fill the dp matrix
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(matrix[i][j]==1)
+                dp[i][j]= 1+ Math.min(Math.min(dp[i][j-1], dp[i-1][j]), dp[i-1][j-1]);
+                ans+= dp[i][j];
+            }
+        }
+
+        return ans;
+    }
+}
